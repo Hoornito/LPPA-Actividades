6638ba22a03ac21d52767261c6b5975afe33257f
Hoornito <agus.fernandez.y@gmail.com>
2022-04-12 17:55:06 -0300

    ejercicio clase 2 - v0.1

---
+++ WebApplicationClase2/WebService1.asmx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplicationClase2
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public bool CUILValidator(int tipo, string dni, int verificador)
+        {
+            bool resultadoVerificador;
+            int x = 0;
+            int suma = 0;
+            int z = 0;
+
+            if (string.IsNullOrEmpty(dni)) throw new ArgumentNullException(nameof(dni));
+            if (dni.Length != 8) throw new ArgumentException(nameof(dni));
+            
+            string cuil = tipo.ToString() + dni + verificador.ToString();
+            int[] multiplicadores = { 5, 4, 3, 2, 7, 6, 5, 4, 3, 2 };
+            while (x < 10) 
+            {
+                int digito = int.Parse(cuil[x].ToString());
+                int digitoValido = digito * multiplicadores[x];
+                suma += digitoValido;
+
+                x++;
+            }
+
+            z = suma % 11;
+            int resultado = 11 - z;
+            resultadoVerificador = (resultado == verificador);
+            
+            return resultadoVerificador;
+        }
+    }
+}
---
+++ WindowsFormsClase2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsClase2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new Service1.WebService1SoapClient();
+
+            var result = client.CUILValidator(Convert.ToInt32(Tipo_txtBox.Text), DNI_txtBox.Text, Convert.ToInt32(DV_txtBox.Text));
+
+            if (result)
+            {
+                MessageBox.Show("El CUIL es valido");
+            }
+            else
+            {
+                MessageBox.Show("El CUIL es invalido");
+            }
+        }
+    }
+}
